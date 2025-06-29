587590acf00c22ad358bf99fb40108e606a77c63
GO5IT <gofiveit@outlook.com>
2025-06-29 16:11:56 +0200

    Fix Expo App Startup Issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,8 @@ export default function RootLayout() {
     <>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="article" options={{ headerShown: false }} />
+        <Stack.Screen name="podcast" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="light" />
